465deb57bde8b89ddce9f7822d8dd96f074e9c7b
AlexanderJara95 <luisalexanderjara95@gmail.com>
2022-11-14 13:50:19 -0500

    Agregando Usuario con 0 órdenes

--- src/views/ordenVenta/OrdenVenta.js
+++ src/views/ordenVenta/OrdenVenta.js
@@ -233,24 +233,7 @@ class OrdenVenta extends Component {
         this.setState({ ordenSeleccionada: [] });
     }
 
-    imprimirTicket = (orden) =>{
-        //const response = store.dispatch(anularOrden());
-        
-        //console.log("this.state.listaUsuarios",this.state.listaUsuarios);
-        //const newWin = window.open('', '', 'height=500, width=1000');
-        //var newWin = document.getElementById('printInfo').innerHTML;
-        /*newWin.document.write('<title>Informe Orden</title>');
-        newWin.document.write('<h2>Orden Finalizada</h2>');
-        newWin.document.write('<div><b>Pedido Ventas:</b> '+orden.pedidoVentas+'</div>');
-        newWin.document.write('<div><b>Envío:</b> '+orden.envio+'</div>');
-        newWin.document.write('<div><b>Nombre Cliente:</b> '+orden.nombreCliente+'</div>');
-        newWin.document.write('<div><b>Referencia:</b> '+orden.referencia+'</div>');
-        newWin.document.write('<div><b>Fecha Emisión:</b> '+orden.emitido+'</div>');
-        newWin.document.write('<div><b>Asignado:</b> '+picker[0].nombre+' '+picker[0].apellido+'</div>');*/
-        //newWin.document.close();
-        //newWin.print();
-          //console.log('print');  
-        
+    imprimirTicket = (orden) =>{        
         setTimeout(() => {
             let printContents = document.getElementById('printInfo').innerHTML;
             document.body.innerHTML = printContents;
